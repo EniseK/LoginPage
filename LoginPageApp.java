03ac38df8d659f0373343d4ba95a1bc1b125fbaa
EniseK <enisekizilaslan@gmail.com>
2023-12-10 15:21:01 +0100

    formed necessary classes

---
+++ LoginPageApp.java
@@ -0,0 +1,37 @@
+public class LoginPageApp {
+/*Project: Design a log-in and sign-up page.
+
+menu: Show the users an options menu
+
+register: Get first name, last name, e-mail address, and password info from the user
+
+login: enter email and password
+if the email's not found, give a notification saying "this email address is not registered on our system, please sign-up.
+if the email and the password are correct, they can enter the website.
+
+email validation: .no space
+                  .must include '@'
+                  .must end with gmail.com or yahoo.com etc.
+                  .before @ there can only be letters, numbers, or '-' , '.' , '_' symbols.
+
+password validation: .no space
+                     .at least 6 characters
+                     .at least 1 lower case letter
+                     .at least 1 uppercase letter
+                     .at least 1 number
+                     .at least 1 symbol
+
+    */
+    public static void main(String[] args) {
+
+start();
+   // a User class for common properties of users
+   //a UserService class for other user operations
+
+
+    }
+
+    private static void start() {
+    }
+
+}
---
+++ UserService.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserService {
+
+    List<String> emails = new ArrayList<>();
+    List<String> passwords = new ArrayList<>();
+
+   // List<User> users = new ArrayList<>();
+
+
+}
