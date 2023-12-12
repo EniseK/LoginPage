01b42b2b9a3ca3b6724097cc0216865929ac1c3f
EniseK <enisekizilaslan@gmail.com>
2023-12-12 19:24:57 +0100

    Added the password section

--- UserService.java
+++ UserService.java
@@ -13,11 +13,13 @@ public class UserService {
         System.out.println("Please enter your full name: ");
         String fullName = inp.nextLine();
 //if the email is faulty, it needs to be entered again. Therefore, it is best to use a do-while loop.
+        String email;
         boolean isValid;
         boolean isExist;
+
         do {
             System.out.println("Please enter your email address: ");
-            String email = inp.nextLine();
+           email = inp.nextLine().trim();
             isValid = validateEmail(email);
             isExist = this.emails.contains(email);
             if (isExist) {
@@ -28,11 +30,31 @@ public class UserService {
         } while (!isValid);
 
 //password creation.
+String password;
+boolean isValidPassword;
 
+do {
+    System.out.println("Please enter a password: ");
+    password=inp.nextLine().trim();
+
+    isValidPassword = validatePassword(password);
+}while (!isValidPassword);
+
+User user =new User(fullName, email, password);
+
+this.emails.add(user.email);
+this.passwords.add(user.password);
+
+        System.out.println("Congratulations! You have successfully signed up.");
+        System.out.println("You can log-in with your email address and password.");
 
     }
 
     private boolean validateEmail(String email) {
+        return true;
+    }
+
+    private boolean validatePassword(String password) {
         return true;
     }
 
